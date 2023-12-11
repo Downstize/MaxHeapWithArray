b3b83cd40a8d6b6acf3222adc8352a93f542bfe8
downstize <swankydid@gmail.com>
2023-12-11 13:56:48 +0300

    init

---
+++ AbstractHeap.cs
@@ -0,0 +1,6 @@
+namespace MaxHeapWithArray;
+
+public interface AbstractHeap
+{
+    
+}
---
+++ MaxHeap.cs
@@ -0,0 +1,11 @@
+namespace MaxHeapWithArray;
+
+public class Heap
+{
+    private List<int> heap;
+
+    public Heap()
+    {
+        
+    }
+}
